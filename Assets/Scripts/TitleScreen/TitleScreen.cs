ed5386e55e7184c99c51a029e60b98ff7af94404
Andrew Bodnar <232428806+abodnar3@users.noreply.github.com>
2026-03-02 17:11:39 -0500

    Added title screen

---
+++ Assets/Scripts/TitleScreen/TitleScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public string gameSceneName = "MainGame";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
